b0ee63e98bf57a48efa73512be2fb49db3761953
amree <mohd.amree@gmail.com>
2017-09-19 09:34:36 +0800

    Update env settings for test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 ENV["RACK_ENV"] = "test"
 
 require File.expand_path("../../config/environment", __FILE__)
-abort("DATABASE_URL environment variable is set") if ENV["DATABASE_URL"]
 
 require "rspec/rails"
 
