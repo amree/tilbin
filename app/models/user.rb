30b95d32f8046850ba8fae2e06395be20c207698
amree <mohd.amree@gmail.com>
2017-12-14 04:06:01 +0800

    Add devise for authentication

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise(
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
+  )
+end
